232f9f60cf425d0cd7d475d762a49bd002fbff7a
Katie Bailey <kfslavin@gmail.com>
2020-09-15 08:07:27 -0400

    refactored exercise 6

--- Ruby_Small_Problems/Easy_4/e06.rb
+++ Ruby_Small_Problems/Easy_4/e06.rb
@@ -1,19 +1,15 @@
 def running_total(array)
   a = []
   b = array.shift
-    if b != nil
-      a << b
-      array.each {|item| a << item + a.last}
-      a
-    else
-      []
-    end
+  if !b.nil?
+    a << b
+    array.each { |item| a << item + a.last }
+    a
+  else
+    []
+  end
 end
-
-
-
 p running_total([2, 5, 13]) == [2, 7, 20]
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
 p running_total([3]) == [3]
 p running_total([]) == []
-
